05eccff1eb147c6688f48d1f41e9df545e33b384
Sagid M <kofon95@mail.ru>
2020-03-08 11:08:04 +0300

    Parse arguments in main.py to output lex/ast/result

--- src/main.py
+++ src/main.py
@@ -1,10 +1,36 @@
 from sys import argv
+from argparse import ArgumentParser
 from src.lexer import Lexer
 from src.abstract_tree import build_abstract_tree
 from src.helpers.object_converter import to_pretty_format
+from src.executor import execute_ast_expression
+
+def read_from_file(filename):
+    with open(filename) as f:
+        return f.read()
+
+def run(args):
+    if args.oneline:
+        code = args.oneline
+    else:
+        code = read_from_file(args.filename)
+
+    tokens = Lexer(code).lex()
+    if args.output == 'lex':
+        return tokens
+    ast = build_abstract_tree(tokens)
+    if args.output == 'ast':
+        return to_pretty_format(ast)
+    return execute_ast_expression(ast)
 
 
-code = argv[1]
-tokens = Lexer(code).lex()
-ast = build_abstract_tree(tokens)
-print(to_pretty_format(ast))
+parser = ArgumentParser()
+parser.add_argument('-o', '--output',
+    choices=('lex', 'ast', 'result'),
+    default='result',
+    help='Outputs either tokens or AST')
+parser.add_argument('--oneline', default=None, help='Executes a line instead of a file')
+parser.add_argument('filename', default=None, nargs='?', help='File that should be run')
+args = parser.parse_args(argv[1:])
+
+print(run(args))
